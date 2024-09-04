21f1bac22e507510f3bc1abe7c1e28b0d106e7af
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-09-04 19:21:27 +0100

    files should be ignored

--- cricket-data/obj/Debug/net8.0/cricket-data.AssemblyInfo.cs
+++ cricket-data/obj/Debug/net8.0/cricket-data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cricket-data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278b9902f9da28bd36390a043551d8e25f73aac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cricket-data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cricket-data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
